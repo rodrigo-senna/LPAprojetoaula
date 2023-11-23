52604f706c0227f83caeb5a905a0012dd234c892
Rodrigo <r_senna_poa@hotmail.com>
2023-11-23 10:24:39 -0300

    Signed-off-by: Rodrigo <r_senna_poa@hotmail.com>

---
+++ DiagramaClasseJogo/Class2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Class2:
+    def __init__(self):
+        pass
+
+    def Operation1(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.Attribute1 = None
+        self.window = None
+        self.entity_list = None
+        self.name = None
+
+    def entity_list(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
